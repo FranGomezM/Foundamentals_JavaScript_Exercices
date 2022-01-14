62e658caf0c33b34ac4ae36e042f10d1cfacf4d0
FranGomezM <travecedo991@gmail.com>
2022-01-14 18:30:42 +0000

    solving 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2) || num1 + 1 != Number(num1) + 1 || num2 + 1 != Number(num2) + 1) {
+        return "ERROR";
+    }
+    else {
+        if (num1 < num2){
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        }
+        else if (num2 < num1) {
+            for (let i = num2; i <= num1; i++) {
+                sum += i;
+            }
+        }
+        else {
+            sum = num1;
+        }
+        return sum; 
+    }
 };
 
 // Do not edit below this line

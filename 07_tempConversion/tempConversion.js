4cbdc4a6630fc745505a8ba7057718b1c19f65e0
FranGomezM <travecedo991@gmail.com>
2022-01-14 22:46:47 +0000

    solving 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let fahr = (temp-32) * (5/9);
+  fahr *= 10;
+  fahr = Math.round(fahr);
+  fahr /= 10;
+  return fahr;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let celsius = temp * (9/5) + 32;
+  celsius *= 10;
+  celsius = Math.round(celsius);
+  celsius /= 10;
+  return celsius;
 };
 
 // Do not edit below this line

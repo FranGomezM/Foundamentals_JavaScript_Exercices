1a6acbd715fc9d304432d95caa8912a500d68f1d
FranGomezM <travecedo991@gmail.com>
2022-01-14 22:08:11 +0000

    solving 6th exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap = year / 4;
+    let leapYear = true;
+    if (leap % 1 === 0){
+        if ((year/400) % 1 === 0) {
+            return leapYear;
+        }
+        else if ((year/100) % 1 === 0) {
+            return !leapYear;
+        }
+        return leapYear;
+    }
+    return !leapYear;
 };
 
 // Do not edit below this line
